78026e20a6751699e56ea6fb56d1e49b30d5618c
xjahic <jahic.jakub@gmail.com>
2024-01-14 16:58:27 +0100

    Use Lombok

--- src/main/java/sk/streetofcode/taskmanagementsystem/domain/Project.java
+++ src/main/java/sk/streetofcode/taskmanagementsystem/domain/Project.java
@@ -1,31 +1,14 @@
 package sk.streetofcode.taskmanagementsystem.domain;
 
+import lombok.Value;
+
+import java.time.OffsetDateTime;
+
+@Value
 public class Project {
-    private final long id;
-    private final long userId;
-    private final String name;
-    private final String description;
-
-    public Project(long id, long userId, String name, String description) {
-        this.id = id;
-        this.userId = userId;
-        this.name = name;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    long id;
+    long userId;
+    String name;
+    String description;
+    OffsetDateTime createdAt;
 }
--- src/main/java/sk/streetofcode/taskmanagementsystem/domain/User.java
+++ src/main/java/sk/streetofcode/taskmanagementsystem/domain/User.java
@@ -1,25 +1,10 @@
 package sk.streetofcode.taskmanagementsystem.domain;
 
+import lombok.Value;
+
+@Value
 public class User {
-    private final long id;
-    private final String name;
-    private final String email;
-
-    public User(long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+    long id;
+    String name;
+    String email;
 }
