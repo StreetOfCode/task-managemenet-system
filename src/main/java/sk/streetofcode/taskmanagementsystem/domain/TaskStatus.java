a7aae2d8f781581db1c2ff8d5445de37991f3fd4
xjahic <jahic.jakub@gmail.com>
2024-01-14 16:56:01 +0100

    Create domain classes

---
+++ src/main/java/sk/streetofcode/taskmanagementsystem/domain/TaskStatus.java
@@ -0,0 +1,14 @@
+package sk.streetofcode.taskmanagementsystem.domain;
+
+public enum TaskStatus {
+    NEW,
+    DONE;
+
+    public static TaskStatus fromString(String status) {
+        return switch (status) {
+            case "NEW" -> NEW;
+            case "DONE" -> DONE;
+            default -> throw new IllegalArgumentException("Unknown task status: " + status);
+        };
+    }
+}
