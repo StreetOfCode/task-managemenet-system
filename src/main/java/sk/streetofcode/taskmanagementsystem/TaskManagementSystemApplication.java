5ff2e9307ef7abdfe87df4cbd1adda7d9e741e41
xjahic <jahic.jakub@gmail.com>
2024-02-09 15:29:16 +0100

    Add OpenApiDefinition annotation

--- src/main/java/sk/streetofcode/taskmanagementsystem/TaskManagementSystemApplication.java
+++ src/main/java/sk/streetofcode/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -1,9 +1,12 @@
 package sk.streetofcode.taskmanagementsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class TaskManagementSystemApplication {
 
 	public static void main(String[] args) {
